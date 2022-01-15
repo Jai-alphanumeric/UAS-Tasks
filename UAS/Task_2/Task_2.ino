08d2f92830f203e110865eb1ac4bd77d6f58e98a
Jai-alphanumeric <91368981+Jai-alphanumeric@users.noreply.github.com>
2022-01-15 18:14:51 +0530

    Update Task_2.ino

--- UAS/Task_2/Task_2.ino
+++ UAS/Task_2/Task_2.ino
@@ -5,6 +5,7 @@ int value=0;
 void setup() {
 Serial.begin(9600);
 pinMode(11,OUTPUT);
+pinMode(A0,INPUT);
 }
 
 void loop() {
@@ -14,30 +15,30 @@ Serial.println(value);
 if(value<256)
   {
     digitalWrite(11,HIGH);                //Makes the LED glow in Dark.
-    delay(1000);
+    delay(3000);
     digitalWrite(11,LOW);
-    delay(1000);
+    delay(3000);
   }
   else if (value>=256 && value<512)
   {
     digitalWrite(11, HIGH);
-    delay(500);
+    delay(2000);
     digitalWrite(11,LOW);
-    delay(500);
+    delay(2000);
 
   }
   else if (value>=512 && value<768)
   {
     digitalWrite(11, HIGH);
-    delay(250);
+    delay(1000);
     digitalWrite(11,LOW);
-    delay(250);
+    delay(1000);
   }
   else
   {
     digitalWrite(11,HIGH);                 //Turns the LED OFF in Light.
-    delay(125);
+    delay(500);
     digitalWrite(11,LOW);
-    delay(125);
+    delay(500);
   }
 }
